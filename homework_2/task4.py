34087df27479f1ae588daeed3e60985ffdfa11a1
Oleh Yarmoliuk <yarmolyuk18@gmail.com>
2020-06-12 00:06:15 +0300

    Task4 added

---
+++ homework_2/task4.py
@@ -0,0 +1,8 @@
+from math import pi
+r = int(input("Enter a radius "))
+A = pi * r ** 2
+d = 2 * r 
+C = pi * d
+print("Area of the circle is " + str(A))
+print("Perimeter of the circle is " + str(C))
+        
