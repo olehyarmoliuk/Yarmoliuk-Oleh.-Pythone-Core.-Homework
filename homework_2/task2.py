68abe602afdc3a5459e7ded0d876ef1ed1185563
Oleh Yarmoliuk <yarmolyuk18@gmail.com>
2020-06-12 00:05:56 +0300

    Task2 added

---
+++ homework_2/task2.py
@@ -0,0 +1,5 @@
+numBytes = input('enter a digit ')
+numKilobytes = int(numBytes) * 2 ** 10
+numMegabytes = int(numBytes) * 2 ** 20
+print(str(numKilobytes) + ' bytes in 1 kilobyte')
+print(str(numMegabytes) + ' bytes in 1 megabyte')
