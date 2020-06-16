58ae88d2b8ffd0b3bb3b6d462ac0bb4cc0e30456
Oleh Yarmoliuk <yarmolyuk18@gmail.com>
2020-06-16 16:55:13 +0300

    task6 added

---
+++ homework_3/task6.py
@@ -0,0 +1,19 @@
+p = int(input("Enter number of place "))
+
+if p in range(1, 37):
+    print("Compartment")
+else: 
+    p in range(37, 55)
+    print("Side")
+
+if p in range (1, 54, 2):
+    print("Bottom")
+else: 
+    p in range (2, 55, 2)
+    print("Top")
+
+if p in range (1, 5) and (53, 55):
+    print("Near conductor")
+else:
+    p in range (33, 37) and (37, 39)
+    print("Near toilet")
