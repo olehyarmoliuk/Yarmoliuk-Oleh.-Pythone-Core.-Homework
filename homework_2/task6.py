8b54dba4d550ee719a2d337c04092781318db500
Oleh Yarmoliuk <yarmolyuk18@gmail.com>
2020-06-16 16:53:30 +0300

    task6 and task7 added

---
+++ homework_2/task6.py
@@ -0,0 +1,8 @@
+collection = ['Cats', 'Cars', 'Elephants']
+print(collection)
+
+description = {'Cats' : 'funny', 'Cars' : 'expensive', 'Elephants' : 'weird'}
+print(description)
+
+cats_unique_marks = {'British shorthair', 'Siamese', 'Persian', 'Maine Coon', 'British shorthair', 'Siamese'}
+print(cats_unique_marks)
---
+++ homework_2/task7.py
@@ -0,0 +1,16 @@
+# There is a collection of cars from 
+# different companies
+
+# You should create a list (mutable data type), 
+# or a tuple if you want it to be immutable
+collection = ['Lamborghini', 'Ford', 'Renault']
+print(collection)
+
+# Then, for instance, specify the country of cars origin
+coll_discript = {'Lamborghini' : 'Italy', 'BMW' : 'Germany', 'Renault' : 'France'}
+print(coll_discript)
+
+# Possibly, you may have same car models in Lamborghini collection
+# To hold information only about unique vehicles use 'set'
+unique_cars = {'Aventador', 'Urus', 'Gallardo', 'Aventador', 'Murcielago', 'Urus', 'Reventon'}
+print(unique_cars)
