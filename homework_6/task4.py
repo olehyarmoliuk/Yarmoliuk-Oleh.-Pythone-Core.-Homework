a0004fc320aff1cc59b4d28222069cecf2519977
Oleh Yarmoliuk <yarmolyuk18@gmail.com>
2020-07-07 17:42:43 +0300

    Task 4 added

---
+++ homework_6/task4.py
@@ -0,0 +1,27 @@
+mtrx = [[1, 2, 3,], 
+        [4, 5, 6], 
+        [7, 8, 9]]
+      
+rows = len(mtrx)
+columns = len(mtrx[0])
+
+sum_rows = [sum(i) for i in mtrx]
+print(sum_rows)
+
+
+sum_columns = [sum(row[i] for row in mtrx) for i in range(len(mtrx[0]))]
+print(sum_columns)
+
+mtrx[0].append(sum_rows[0])
+mtrx[1].append(sum_rows[1])
+mtrx[2].append(sum_rows[2])
+mtrx.append(sum_columns)
+mtrx[3].append(sum_rows[0] + sum_rows[1] + sum_rows[2])
+
+print(mtrx)
+
+
+[[1, 2, 3, 6], 
+[4, 5, 6, 15], 
+[7, 8, 9, 24], 
+[12, 15, 18, 45]]
