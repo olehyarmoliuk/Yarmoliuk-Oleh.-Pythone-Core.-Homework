7fcc4039af56a13b5e8d3ff28969a40c49499912
olehyarmoliuk <66630576+olehyarmoliuk@users.noreply.github.com>
2020-07-23 13:16:50 +0300

    Update task6.py

--- homework_4/task6.py
+++ homework_4/task6.py
@@ -1,14 +1,16 @@
 pos_num, neg_num = 0, 0
 
 while True:
-    num_1 = int(input('Enter your number '))
+    
+    num_1 = int(input('Enter your number. To stop inputting "press 0" '))
  
     if num_1 > 0:
         pos_num += 1
     elif num_1 < 0:
         neg_num += 1
-    else:
-        per = 100/(pos_num + neg_num)
-        print("Percentage of positive numbers: ", int(pos_num * per))
-        print("Percentage of negative numbers: ", int(neg_num * per))
-        break
+    else: 
+        if num_1 == 0:
+            per = 100/(pos_num + neg_num)
+            print("Percentage of positive numbers: ", int(pos_num * per))
+            print("Percentage of negative numbers: ", int(neg_num * per))
+            break
